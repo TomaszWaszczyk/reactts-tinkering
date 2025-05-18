30985d8737919fac5a32f8deb2b4227d428375e7
Tomasz Waszczyk <tomasz@waszczyk.com>
2025-05-18 17:46:54 +0200

    added countdown

---
+++ src/components/Countdown/index.tsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+
+function Countdown({ initialCount = 10 }: { initialCount?: number }) {
+  const [count, setCount] = useState(initialCount);
+
+  useEffect(() => {
+    if (count <= 0) return;
+    const timerId = setInterval(() => {
+      setCount((c: number) => c - 1);
+    }, 1000);
+
+    return () => clearInterval(timerId);
+  }, [count]);
+
+  return (
+    <div>
+      <h1>Countdown: {count}</h1>
+    </div>
+  );
+}
+
+export default Countdown;
